abe35405ce220e6551ccd4902860396602ceb50b
Administrator <Administrator@USER-8A2PPMEBM7>
2018-08-23 00:53:06 +0800

    工具提交

---
+++ src/main/java/platflat/common/configure/ConnectionPropertiesConfigure.java
@@ -0,0 +1,42 @@
+package platflat.common.configure;
+
+import java.io.Serializable;
+
+public class ConnectionPropertiesConfigure implements Serializable{
+	private static final long serialVersionUID = -5753081759963474760L;
+	private String url;
+	private int port;
+	private String contextPath;
+	private String user;
+	private String password;
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getContextPath() {
+		return contextPath;
+	}
+	public void setContextPath(String contextPath) {
+		this.contextPath = contextPath;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/platflat/common/configure/SystemPropertiesConfigure.java
@@ -0,0 +1,46 @@
+package platflat.common.configure;
+
+import java.io.Serializable;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+/**
+ * 系统配置
+ * @ClassName: SystemPropertiesConfigure   
+ * @Description: 环境变量配置  
+ * @author: Administrator  
+ * @date:2018年8月23日 上午12:36:59
+ */
+@Component  
+@ConfigurationProperties(prefix="system") 
+public class SystemPropertiesConfigure implements Serializable{
+	private static final long serialVersionUID = 4170140471320327958L;
+	private ConnectionPropertiesConfigure oracleConnection;
+    private ConnectionPropertiesConfigure mySqlConnection;
+    private ConnectionPropertiesConfigure kafkaConnection;
+    private ConnectionPropertiesConfigure redisConnection;
+	public ConnectionPropertiesConfigure getOracleConnection() {
+		return oracleConnection;
+	}
+	public void setOracleConnection(ConnectionPropertiesConfigure oracleConnection) {
+		this.oracleConnection = oracleConnection;
+	}
+	public ConnectionPropertiesConfigure getMySqlConnection() {
+		return mySqlConnection;
+	}
+	public void setMySqlConnection(ConnectionPropertiesConfigure mySqlConnection) {
+		this.mySqlConnection = mySqlConnection;
+	}
+	public ConnectionPropertiesConfigure getKafkaConnection() {
+		return kafkaConnection;
+	}
+	public void setKafkaConnection(ConnectionPropertiesConfigure kafkaConnection) {
+		this.kafkaConnection = kafkaConnection;
+	}
+	public ConnectionPropertiesConfigure getRedisConnection() {
+		return redisConnection;
+	}
+	public void setRedisConnection(ConnectionPropertiesConfigure redisConnection) {
+		this.redisConnection = redisConnection;
+	}
+}
