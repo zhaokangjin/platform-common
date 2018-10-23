7e5a8e712149563c3f1451e9e74c451848b5033a
kangjin.zhao@accenture <kangjin.zhao@accenture@zkjstar>
2018-10-23 23:39:25 +0800

    Example增强

--- src/main/java/com/platform/configure/base/ConditionToExample.java
+++ src/main/java/com/platform/configure/base/ConditionToExample.java
@@ -1,7 +1,7 @@
 package com.platform.configure.base;
 
-public class ConditionToExample<E, C> {
-	public  E getExample(E example, C condition) {
+public class ConditionToExample<E, C, T> {
+	public  E getExample(E example, C condition,T criteria) {
 		return example;
 	}
 }
