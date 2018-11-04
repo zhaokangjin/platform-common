55b14ba52df12825f1d63da7c93fad4c4d8d9a95
kangjin.zhao@accenture <kangjin.zhao@accenture@zkjstar>
2018-11-05 01:09:09 +0800

    锁风险和事务回滚标记

--- src/main/java/com/platform/configure/base/BaseService.java
+++ src/main/java/com/platform/configure/base/BaseService.java
@@ -2,34 +2,40 @@ package com.platform.configure.base;
 
 import java.util.List;
 
+import com.platform.configure.base.annotation.LockRisk;
+
 /**
  * 
- * @author kangjin.zhao@accenture
- *
+ * @author kangjin.zhao
  * @param <R> Record 数据库表字段和对象之间的映射
  * @param <C> Condition 调用参数
- * @param <I> in or not in 调用参数(in 子查询，可能是字符串，日期，数字等)
  */
 public interface BaseService<R, C> {
 
 	public StatusResult<Long> countByExample();
-
+	
+	@LockRisk
 	public StatusResult<Integer> deleteByExample();
 
+	@LockRisk
 	public StatusResult<Integer> insert(R record);
 
+	@LockRisk
 	public StatusResult<Integer> insertSelective(R record);
 	
 	public StatusResult<List<R>> selectByExample(C condition);
 	//支持分页
 	public StatusResult<List<R>> selectByExample();
 
+	@LockRisk
 	public StatusResult<Integer> updateByExampleSelective(R record);
 
+	@LockRisk
 	public StatusResult<Integer> updateByExample(R record);
 
+	@LockRisk
 	public StatusResult<Integer> bathcDeleteUpdateOrInsertByExample(List<R> record, C conditon);
-	//@Param("fieldName") String fieldName, @Param("paramList") List<String> paramList, @Param("splitSize") Integer splitSize, @Param("example") FieldMappingExample example
+
 	public StatusResult<List<R>> selectRecordsByInList(String fieldName, List<Object> paramListIn, Integer splitSize,C conditon);
 
 	public StatusResult<List<R>> selectRecordsByNotInList(String fieldName, List<Object> paramListNotIn, Integer splitSize,C conditon);
---
+++ src/main/java/com/platform/configure/base/annotation/LockRisk.java
@@ -0,0 +1,5 @@
+package com.platform.configure.base.annotation;
+
+public @interface LockRisk {
+
+}
