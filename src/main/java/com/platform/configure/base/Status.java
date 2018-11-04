6a1960c23d890c63b9d4e5b5e26c9cdf3e9b985a
kangjin.zhao@accenture <kangjin.zhao@accenture@zkjstar>
2018-11-05 00:31:59 +0800

    status优化

---
+++ src/main/java/com/platform/configure/base/Status.java
@@ -0,0 +1,6 @@
+package com.platform.configure.base;
+
+public enum Status {
+	//200,   500,       404                         401,         502,        504        
+	Success, Exception, Resource_Not_Found,Time_Out,Login_Failed,Bad_Gateway,Gateway_Timeout 
+}
--- src/main/java/com/platform/configure/base/StatusResult.java
+++ src/main/java/com/platform/configure/base/StatusResult.java
@@ -2,9 +2,7 @@ package com.platform.configure.base;
 
 import java.io.Serializable;
 /**
- * 
- * @author kangjin.zhao@accenture
- *
+ * @author kangjin.zhao
  * @param <R>
  */
 public class StatusResult <R> implements Serializable{
@@ -15,9 +13,25 @@ public class StatusResult <R> implements Serializable{
 	 * 
 	 * @param status
 	 * @param result
+	 * 200,     500,       404                408,     401,         502,        504        
+	   Success, Exception, Resource_Not_Found,Time_Out,Login_Failed,Bad_Gateway,Gateway_Timeout 
 	 */
-	public StatusResult(int status,R result) {
-		this.status=status;
+	public StatusResult(Status status,R result) {
+		if(status.name().equals("Success")) {
+			this.status=200;
+		}else if(status.name().equals("Exception")) {
+			this.status=500;
+		}else if(status.name().equals("Resource_Not_Found")) {
+			this.status=404;
+		}else if(status.name().equals("Time_Out")) {
+			this.status=408;
+		}else if(status.name().equals("Login_Failed")) {
+			this.status=401;
+		}else if(status.name().equals("Bad_Gateway")) {
+			this.status=502;
+		}else if(status.name().equals("Gateway_Timeout")) {
+			this.status=504;
+		}
 		this.result=result;
 	}
 	public int getStatus() {
