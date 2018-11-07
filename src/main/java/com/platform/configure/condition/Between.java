3f254e636dc81bbb512c788e79c97eacd514ae40
kangjin.zhao@accenture <kangjin.zhao@accenture@zkjstar>
2018-11-07 23:52:59 +0800

    重构条件

--- src/main/java/com/platform/configure/condition/Between.java
+++ src/main/java/com/platform/configure/condition/Between.java
@@ -1,24 +1,20 @@
 package com.platform.configure.condition;
 
-public class BetweenCondition<T> extends BaseCondition{
+public class Between<T> extends BaseCondition{
 	private static final long serialVersionUID = -4249653188402445007L;
 	private T preValue;
 	private T endValue;
-
-
 	public T getPreValue() {
 		return preValue;
 	}
-
 	public void setPreValue(T preValue) {
 		this.preValue = preValue;
 	}
-
 	public T getEndValue() {
 		return endValue;
 	}
-
 	public void setEndValue(T endValue) {
 		this.endValue = endValue;
 	}
+	
 }
--- src/main/java/com/platform/configure/condition/Conditions.java
+++ src/main/java/com/platform/configure/condition/Conditions.java
@@ -1,180 +1,96 @@
 package com.platform.configure.condition;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+/*以下命名尽可能遵守ONGL表达式命名法则*/
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Conditions<T> extends BaseCondition {
 	private static final long serialVersionUID = -4137598697029736184L;
-	private BetweenCondition<T> between;
-	private EqualsCondition<T> equals;
-	private LikeCondition<T> like;
-	private GreaterThanCondition<T> greaterThan;
-	private GreatThanOrEqualToCondition<T> greaterThanOrEquals;
-	private NotInCondition<T> notIn;
-	private LessThanCondition<T> lessThan;
-	private LessThanOrEqualToCondition<T> lessThanOrEquals;
-	private NotEqualsCondition<T> notEquals;
-	/*以下命名遵守ONGL表达式命名法则*/
-	//in
-	private InCondition<T> in;
-	//between
-	private BetweenCondition<T> bw;
-	//==
-	private EqualsCondition<T> eq;
-	//!=
-	private NotEqualsCondition<T> neq;
-	//>
-	private GreaterThanCondition<T> gt;
-	//>=
-	private GreatThanOrEqualToCondition<T> gte;
+	//in 
+	private List<T> in;
 	//not in
-	private NotInCondition<T> nin;
+	private List<T> nin;
+	//like
+	private T lk;
+	//between
+	private Between<T> bw;
+	//==
+	private T eq;
+	//!=
+	private T neq;
+	//>
+	private T gt;
+	//>=
+	private T gte;
 	//< 
-	private LessThanCondition<T> lt;
+	private T lt;
 	//<=
-	private LessThanOrEqualToCondition<T> lte;
-
-	public BetweenCondition<T> getBetween() {
-		return between;
-	}
-
-	public void setBetween(BetweenCondition<T> between) {
-		this.between = between;
-	}
-
-	public EqualsCondition<T> getEquals() {
-		return equals;
-	}
-
-	public void setEquals(EqualsCondition<T> equals) {
-		this.equals = equals;
-	}
-
-	public GreaterThanCondition<T> getGreaterThan() {
-		return greaterThan;
-	}
-
-	public void setGreaterThan(GreaterThanCondition<T> greaterThan) {
-		this.greaterThan = greaterThan;
-	}
-
-	public GreatThanOrEqualToCondition<T> getGreaterThanOrEquals() {
-		return greaterThanOrEquals;
-	}
-
-	public void setGreaterThanOrEquals(GreatThanOrEqualToCondition<T> greaterThanOrEquals) {
-		this.greaterThanOrEquals = greaterThanOrEquals;
-	}
-
-	public InCondition<T> getIn() {
+	private T lte;
+	public List<T> getIn() {
 		return in;
 	}
-
-	public void setIn(InCondition<T> in) {
+	public void setIn(List<T> in) {
 		this.in = in;
 	}
-
-	public NotInCondition<T> getNotIn() {
-		return notIn;
-	}
-
-	public void setNotIn(NotInCondition<T> notIn) {
-		this.notIn = notIn;
-	}
-
-	public LessThanCondition<T> getLessThan() {
-		return lessThan;
-	}
-
-	public void setLessThan(LessThanCondition<T> lessThan) {
-		this.lessThan = lessThan;
-	}
-
-	public LessThanOrEqualToCondition<T> getLessThanOrEquals() {
-		return lessThanOrEquals;
-	}
-
-	public void setLessThanOrEquals(LessThanOrEqualToCondition<T> lessThanOrEquals) {
-		this.lessThanOrEquals = lessThanOrEquals;
-	}
-
-	public NotEqualsCondition<T> getNotEquals() {
-		return notEquals;
-	}
-
-	public void setNotEquals(NotEqualsCondition<T> notEquals) {
-		this.notEquals = notEquals;
-	}
-
-	public LikeCondition<T> getLike() {
-		return like;
-	}
-
-	public void setLike(LikeCondition<T> like) {
-		this.like = like;
-	}
-
-	public BetweenCondition<T> getBw() {
-		return bw;
-	}
-
-	public void setBw(BetweenCondition<T> bw) {
-		this.bw = bw;
-	}
-
-	public EqualsCondition<T> getEq() {
-		return eq;
-	}
-
-	public void setEq(EqualsCondition<T> eq) {
-		this.eq = eq;
-	}
-
-	public GreaterThanCondition<T> getGt() {
-		return gt;
-	}
-
-	public void setGt(GreaterThanCondition<T> gt) {
-		this.gt = gt;
-	}
-
-	public GreatThanOrEqualToCondition<T> getGte() {
-		return gte;
-	}
-
-	public void setGte(GreatThanOrEqualToCondition<T> gte) {
-		this.gte = gte;
-	}
-
-	public NotInCondition<T> getNin() {
+	public List<T> getNin() {
 		return nin;
 	}
-
-	public void setNin(NotInCondition<T> nin) {
+	public void setNin(List<T> nin) {
 		this.nin = nin;
 	}
-
-	public LessThanCondition<T> getLt() {
-		return lt;
+	public T getLk() {
+		return lk;
 	}
-
-	public void setLt(LessThanCondition<T> lt) {
-		this.lt = lt;
+	public void setLk(T lk) {
+		this.lk = lk;
 	}
-
-	public LessThanOrEqualToCondition<T> getLte() {
-		return lte;
+	public Between<T> getBw() {
+		return bw;
 	}
-
-	public void setLte(LessThanOrEqualToCondition<T> lte) {
-		this.lte = lte;
+	public void setBw(Between<T> bw) {
+		this.bw = bw;
 	}
-
-	public NotEqualsCondition<T> getNeq() {
+	public T getEq() {
+		return eq;
+	}
+	public void setEq(T eq) {
+		this.eq = eq;
+	}
+	public T getNeq() {
 		return neq;
 	}
-
-	public void setNeq(NotEqualsCondition<T> neq) {
+	public void setNeq(T neq) {
 		this.neq = neq;
 	}
+	public T getGt() {
+		return gt;
+	}
+	public void setGt(T gt) {
+		this.gt = gt;
+	}
+	public T getGte() {
+		return gte;
+	}
+	public void setGte(T gte) {
+		this.gte = gte;
+	}
+	public T getLt() {
+		return lt;
+	}
+	public void setLt(T lt) {
+		this.lt = lt;
+	}
+	public T getLte() {
+		return lte;
+	}
+	public void setLte(T lte) {
+		this.lte = lte;
+	}
+	@Override
+	public String toString() {
+		return "Conditions [in=" + in + ", nin=" + nin + ", lk=" + lk + ", bw=" + bw + ", eq=" + eq + ", neq=" + neq
+				+ ", gt=" + gt + ", gte=" + gte + ", lt=" + lt + ", lte=" + lte + "]";
+	}
+
 }
